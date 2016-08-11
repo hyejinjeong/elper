f81c9038faa61baef3e22bd2914d538ee347af82
hyejin <gpwls102030@gmail.com>
2016-08-12 01:16:36 +0900

    add event's index, show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_event, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@time_slots = current_user.time_slots
@@ -9,6 +10,7 @@ class EventsController < ApplicationController
 	def create
 		@event = Event.new(event_params)
 		@event.user_id = current_user
+
 		if @event.save
 			redirect_to events_path
 		else
@@ -16,12 +18,34 @@ class EventsController < ApplicationController
 		end
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
 	def index
 		@events = Event.all
 		@events = Event.order(created_at: :desc)
 	end
 
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+
+		if @event.update(event_params)
+			redirect_to @event
+		else
+			render 'edit'
+		end
+	end
+
 	private
+	def set_event
+		@event = Event.find(params[:id])
+	end
+
 	def event_params
 		params.require(:event).permit(:title, :start_date, :period)
 	end
